50e936646c8c4fb30e03f53259f004db4dbab8b5
askvart <tur1amant@gmail.com>
2021-11-08 19:48:01 +0300

    vendor/urls.py path(vendor-admin/)

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -5,5 +5,6 @@ from apps.vendor import views
 
 urlpatterns = [
     path('become-vendor/', views.become_vendor, name='become_vendor'),
+    path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
     path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login'),
 ]
