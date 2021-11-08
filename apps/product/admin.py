17d30055abf683d076ea1a5c71cb007cd951969a
askvart <tur1amant@gmail.com>
2021-11-08 22:58:22 +0300

    apps/product/templates/parts/list_item.html +{url product}

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from apps.product.models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category', 'vendor', 'title', 'price')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
