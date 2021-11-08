8ad3cc3651f3dc136e53923f430320d00a57af80
askvart <tur1amant@gmail.com>
2021-11-08 18:56:17 +0300

    core.views.py def frontpage, def contacr

---
+++ apps/core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def frontpage(request):
+    return render(request, 'core/frontpage.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
