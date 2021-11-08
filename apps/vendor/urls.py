ed91d4ed8ecec8455c31a7bed696357d75c4dda1
askvart <tur1amant@gmail.com>
2021-11-08 19:53:23 +0300

    vendor/urls.py logout

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -6,5 +6,6 @@ from apps.vendor import views
 urlpatterns = [
     path('become-vendor/', views.become_vendor, name='become_vendor'),
     path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login'),
 ]
