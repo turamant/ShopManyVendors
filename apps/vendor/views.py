999a6fa4be06527aa054940cf689e1828e2ea98e
askvart <tur1amant@gmail.com>
2021-11-08 19:15:35 +0300

    vendor/views.py

---
+++ apps/vendor/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from apps.vendor.models import Vendor
+
+def become_vendor(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            vendor = Vendor.objects.create(name=user.username, created_by=user)
+
+            return redirect('frontpage')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'vendor/become_vendor.html', {'form': form})
