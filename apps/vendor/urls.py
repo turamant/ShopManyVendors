1d2b52c6c70b355cd133f52f275416bee1c82ce9
askvart <tur1amant@gmail.com>
2021-11-08 19:31:12 +0300

    vendor/urls.py login

--- apps/vendor/urls.py
+++ apps/vendor/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from apps.vendor import views
 
 
 urlpatterns = [
     path('become-vendor/', views.become_vendor, name='become_vendor'),
+    path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='login'),
 ]
