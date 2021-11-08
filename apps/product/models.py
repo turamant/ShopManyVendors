89a52714c0abcfb38b0e427c0c8d43f765ff7281
askvart <tur1amant@gmail.com>
2021-11-08 20:10:47 +0300

    apps/product/models.py +Category

---
+++ apps/product/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['ordering']
+
+    def __str__(self):
+        return self.title
