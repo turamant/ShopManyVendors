39939b55e1cdd066ed7d071e54c69b1d61d33a02
askvart <tur1amant@gmail.com>
2021-11-09 14:57:34 +0300

    configShop/settings.py + cont_proces

--- configShop/settings.py
+++ configShop/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.product.context_processors.menu_categories',
+                'apps.cart.context_processors.cart',
             ],
         },
     },
