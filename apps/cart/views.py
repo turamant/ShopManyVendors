5795931f7ee113b8d9efde7eb23e76ec7ea1e661
askvart <tur1amant@gmail.com>
2021-11-09 15:38:28 +0300

    apps/cart/views.py + change_quantity

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -6,9 +6,14 @@ from apps.cart.cart import Cart
 def cart_detail(request):
     cart = Cart(request)
     remove_from_cart = request.GET.get('remove_from_cart', '')
+    change_quantity = request.GET.get('change_quantity', '')
+    quantity = request.GET.get('quantity', 0)
 
     if remove_from_cart:
         cart.remove(remove_from_cart)
+        return redirect('cart')
 
+    if change_quantity:
+        cart.add(change_quantity, quantity, True)
         return redirect('cart')
     return render(request, 'cart/cart.html')
