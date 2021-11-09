8861af0d32e8fd2c763cfcddd51f667e0b1fb800
askvart <tur1amant@gmail.com>
2021-11-09 14:37:42 +0300

    product/forms.py +AddCartForm

--- apps/product/views.py
+++ apps/product/views.py
@@ -3,6 +3,7 @@ import random
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
+from apps.cart.cart import Cart
 from apps.product.models import Product, Category
 
 
@@ -13,9 +14,11 @@ def search(request):
     return render(request, 'product/search.html', {'products': products, 'query': query})
 
 def product(request, category_slug, product_slug):
-
+    cart = Cart(request)
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
 
+    if request.method == "POST":
+        form = AddToCartForm
 
     similar_products = list(product.category.products.exclude(id=product.id))
 
