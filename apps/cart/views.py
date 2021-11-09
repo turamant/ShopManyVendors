39633a5dbb79c64c9b3e0d9c85884530f80b2b21
askvart <tur1amant@gmail.com>
2021-11-09 15:32:12 +0300

    apps/cart/views.py +cart_detail()

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from apps.cart.cart import Cart
 
 
 def cart_detail(request):
+    cart = Cart(request)
+    remove_from_cart = request.GET.get('remove_from_cart', '')
+
+    if remove_from_cart:
+        cart.remove(remove_from_cart)
+
+        return redirect('cart')
     return render(request, 'cart/cart.html')
