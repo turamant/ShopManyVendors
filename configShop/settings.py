2cd06c10bacd337e37a4921af2b8fc9d4ee3eb41
askvart <tur1amant@gmail.com>
2021-11-11 01:10:17 +0300

    settings

--- configShop/settings.py
+++ configShop/settings.py
@@ -45,15 +45,6 @@ CART_SESSION_ID = 'cart'
 
 
 
-STRIPE_PUB_KEY = 'pk_test_51IaGrcHeqmgb1QiuG622WhjRNTSICaqPuSAmr8xHf2NU7s61CWqWofWjxD6lJEeJUDUED9Vv9FFvFihrGTOINZNq00IlAF4Bcm'
-STRIPE_SECRET_KEY ='sk_test_51IaGrcHeqmgb1QiuAPQVtpvRrfNXDhOYHrpREKSjKf7MbVEgVhSNpZXVHeOD2ojNVDWuSs23tbsSeU6RZr0Lk78500pdAirsRO'
-
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_HOST_USER = 'viktoraskvart@yandex.ru'
-EMAIL_HOST_PASSWORD = 'KkXkJHPJQ7g3L5e'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = True
-DEFAULT_EMAIL_FROM = 'ShopManyVendors <noreply@viktoraskvart@yandex.ru>'
 
 # Application definition
 
